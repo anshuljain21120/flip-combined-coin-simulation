f0af0f3ed307489b887dac1ba122049d4424cbe8
Anshul Jain <anshuljain21120@gmail.com>
2020-07-14 18:20:44 +0530

    build basic coin flip

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,3 +2,15 @@
 
 echo "Welcome to Flip Combined Coin Simulation!";
 echo "This problem displays winning percentage of Head or Tail combination in a single, Doublet or Triplet";
+
+function flip_a_coin()
+{
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		echo "Head";
+	else
+		echo "Tail";
+	fi
+}
+
+echo "$( flip_a_coin )";
